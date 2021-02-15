370bcea597db54ef5b23a9a20427305de253b212
Lukas Jaworski <ljaworski88@gmail.com>
2021-02-14 21:02:15 -0500

    merged the soil moisture sensor and fixed some api interations as the api of certain libraries changed

--- topoff/src/python/topoff.py
+++ topoff/src/python/topoff.py
@@ -15,8 +15,8 @@ spi = spidev.SpiDev()
 SPI_CHANNEL = 0 # The hardware spi channel that the mcp3008 is attached to
 TANK_LEVEL_FLOAT = 4 # The R.Pi pin which is connected to the float switch (monitors tank low level)
 TANK_TOPOFF_PUMP = 14 # The R.Pi pin which controls the relay to the top off pump
-MIN_WATER_READING = 465 # The reading of the water gauge when empty
-MAX_WATER_READING = 962 # The reading of the water gauge when full
+MIN_WATER_READING = 582 # The reading of the water gauge when empty
+MAX_WATER_READING = 952 # The reading of the water gauge when full
 BY_PERCENTAGE = [0, 100] # Used in interp mapping
 TOPOFF_LEVEL = [MIN_WATER_READING, MAX_WATER_READING] # Used in interp mapping
 
@@ -38,22 +38,46 @@ def top_off():
 
     global water_tank_empty
     fill_error = False
+    check_water_tank()
     gpio.setup(TANK_LEVEL_FLOAT, gpio.IN, pull_up_down=gpio.PUD_UP)
+
+    # setting up for topoff and checking the water tank
     if not gpio.input(TANK_LEVEL_FLOAT) and not water_tank_empty:
         print('{}:top off started'.format(datetime.datetime.now(eastern).strftime('%Y/%m/%d-%H:%M:%S')))
         start_time = datetime.datetime.now(eastern)
+        run_pump_timer = datetime.datetime.now(eastern)
         gpio.output(TANK_TOPOFF_PUMP, gpio.LOW)
+
+        # the topping off portion of the code
         while not gpio.input(TANK_LEVEL_FLOAT):
             sleep(5)
+
+            # run the pump for one minute with 15 second breaks in between, just in case the motor is running dry
+            if (datetime.datetime.now(eastern) - run_pump_timer) > datetime.timedelta(seconds=60):
+                gpio.output(TANK_TOPOFF_PUMP, gpio.HIGH)
+                sleep(15)
+                gpio.output(TANK_TOPOFF_PUMP, gpio.LOW)
+                run_pump_timer = datetime.datetime.now(eastern)
+
+            # if the entire topoff is taking longer than an hour, then something is wrong
             if (datetime.datetime.now(eastern) - start_time) > datetime.timedelta(hours=1):
                 print('topoff taking too long check pump system')
                 fill_error = True
                 break
+
+        # make sure to turn off the pump
         gpio.output(TANK_TOPOFF_PUMP, gpio.HIGH)
+
+        # log what happened
         if fill_error:
             print('{}:top off interuppted, check error logs'.format(datetime.datetime.now(eastern).strftime('%Y/%m/%d-%H:%M:%S')))
+
         else:
             print('{}:top off finished'.format(datetime.datetime.now(eastern).strftime('%Y/%m/%d-%H:%M:%S')))
+
+    elif water_tank_empty:
+        print('{}:top off skipped, water tank too low'.format(datetime.datetime.now(eastern).strftime('%Y/%m/%d-%H:%M:%S')))
+
     else:
         print('{}: no top off needed'.format(datetime.datetime.now(eastern).strftime('%Y/%m/%d-%H:%M:%S')))
     gpio.setup(TANK_LEVEL_FLOAT, gpio.IN, pull_up_down=gpio.PUD_OFF)
@@ -77,7 +101,8 @@ def read_mcp3008(channel, mode='differential'):
             mode = 0
         else:
             mode = 1
-    reading = spi.xfer2([1, ((mode << 3) + channel) << 4, 0], speed_hz=1350000)
+    spi.max_speed_hz = 1350000
+    reading = spi.xfer2([1, ((mode << 3) + channel) << 4, 0])
     spi.close()
     return (((reading[1] & 0b11) << 8) + reading[2])
 
@@ -93,7 +118,7 @@ def check_water_tank():
     print('water level is: ' + str(water_level))
     global water_tank_empty
     try:
-        if water_level < 5:
+        if water_level < 30:
             water_tank_empty = True
             print('water tank empty')
         else:
@@ -104,7 +129,7 @@ def check_water_tank():
 
 schedule.every().day.at('01:30').do(top_off)
 # schedule.every().second.do(check_water_tank)
-# schedule.every().hour.do(check_water_tank)
+schedule.every().hour.do(check_water_tank)
 while True:
     schedule.run_pending()
     sleep(3600)
